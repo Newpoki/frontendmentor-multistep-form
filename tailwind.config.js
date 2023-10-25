d3dc944d968815dad50fe741df5b08927df0ecb2
Jason Savelli <jason.savelli@mention.com>
2023-10-25 22:00:55 +0200

    task: add theme base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ['./index.html', './src/**/*.{ts,tsx}'],
+    theme: {
+        fontFamily: {
+            ubuntu: 'Ubuntu',
+        },
+        colors: {
+            blue50: '#EFF5FF',
+            grey200: '#D6D9E6',
+            blue800: '#022959',
+            grey500: '#9699AA',
+            blue300: '#ABBCFF',
+            grey300: '#D6D9E6',
+            orange500: '#FFAF7E',
+            pink500: '#F9818E',
+            purple500: '#483EFF',
+            red500: '#EE374A',
+            blue100: '#BEE2FD',
+            grey100: '#F8F9FF',
+            white: '#FFF',
+            transparent: 'transparent',
+        },
+        extend: {},
+    },
+    plugins: [],
+}
