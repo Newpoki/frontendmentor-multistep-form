315d7a3cfcb4dab803493b280f76115cb720bc43
Jason Savelli <jason.savelli@mention.com>
2023-10-26 22:56:13 +0200

    feat: add transition between wizard steps

--- src/subscription-forms/subscription-forms.tsx
+++ src/subscription-forms/subscription-forms.tsx
@@ -1,3 +1,4 @@
+import { useTransition, animated } from 'react-spring'
 import { SubscriptionFormsPersonalInfos } from './personal-info/subscription-forms-personal-infos'
 import { SubscriptionFormsPlan } from './plan/subscription-forms-plan'
 import { SubscriptionWizardContext } from './use-subcription-wizard'
@@ -7,15 +8,33 @@ type Props = {
 }
 
 export const SubscriptionForms = ({ subscriptionWizard }: Props) => {
-    switch (subscriptionWizard.wizardState.currentStep) {
-        case 'personalInfos':
-            return <SubscriptionFormsPersonalInfos subscriptionWizard={subscriptionWizard} />
+    const { currentStep } = subscriptionWizard.wizardState
 
-        case 'plan':
-            return <SubscriptionFormsPlan subscriptionWizard={subscriptionWizard} />
+    const transitions = useTransition(currentStep, {
+        from: { opacity: 0, transform: 'translate3d(100vw, 0, 0)' } as const,
+        enter: { opacity: 1, transform: 'translate3d(0, 0, 0)' } as const,
+        leave: {
+            opacity: 0,
+            transform: 'translate3d(-100vw, 0, 0)',
+        } as const,
+    })
 
-        // TODO: Must be deleted when all form names are handled
-        default:
-            throw new Error(`unknown form name: ${subscriptionWizard.wizardState.currentStep}`)
-    }
+    return (
+        <div className="relative flex flex-1 flex-col overflow-hidden">
+            {transitions((props, item) => (
+                <animated.div
+                    style={props}
+                    // absolute is needed here due to how springs works in DOM to animate elements
+                    className="absolute bottom-0 left-0 right-0 top-0 flex flex-1 flex-col"
+                >
+                    {item === 'personalInfos' && (
+                        <SubscriptionFormsPersonalInfos subscriptionWizard={subscriptionWizard} />
+                    )}
+                    {item === 'plan' && (
+                        <SubscriptionFormsPlan subscriptionWizard={subscriptionWizard} />
+                    )}
+                </animated.div>
+            ))}
+        </div>
+    )
 }
