a3ce7d3a9dc603ebaf3308db8ed92eab23813807
Jason Savelli <jason.savelli@mention.com>
2023-10-26 08:46:41 +0200

    feat: create textfield component

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,36 @@
 import { Button } from './components/button'
 import { StpeIndicator } from './components/step-indicator'
+import { Textfield } from './components/textfield'
 
 export const App = () => {
     return (
-        <main className="desktop:items-center desktop:justify-center flex flex-1 flex-col">
-            <div className="bg-mobile desktop:hidden fixed h-[172px] w-full bg-cover" />
+        <main className="flex flex-1 flex-col desktop:items-center desktop:justify-center">
+            <div className="fixed h-[172px] w-full bg-mobile bg-cover desktop:hidden" />
 
-            <div className="desktop:flex-row desktop:w-[940px] desktop:rounded-[15px] desktop:bg-white  desktop:shadow-default desktop:m-[105px] relative flex flex-1 flex-col">
-                <header className="desktop:flex-col desktop:bg-desktop desktop:bg-cover desktop:rounded-[10px] desktop:items-start desktop:justify-start desktop:py-10 desktop:px-8 desktop:gap-8 desktop:w-[274px] desktop:ml-4 desktop:my-4 flex items-center justify-center gap-4 py-8">
+            <div className="relative flex flex-1 flex-col  desktop:m-[105px] desktop:w-[940px] desktop:flex-row desktop:rounded-[15px] desktop:bg-white desktop:shadow-default">
+                <header className="flex items-center justify-center gap-4 py-8 desktop:my-4 desktop:ml-4 desktop:w-[274px] desktop:flex-col desktop:items-start desktop:justify-start desktop:gap-8 desktop:rounded-[10px] desktop:bg-desktop desktop:bg-cover desktop:px-8 desktop:py-10">
                     <StpeIndicator stepNumber={1} isCurrentStep title="your info" />
                     <StpeIndicator stepNumber={2} isCurrentStep={false} title="select plan" />
                     <StpeIndicator stepNumber={3} isCurrentStep={false} title="add-ons" />
                     <StpeIndicator stepNumber={4} isCurrentStep={false} title="summary" />
                 </header>
 
-                <div className="desktop:px-[100px] desktop:py-14 desktop:pb-8 flex flex-1 flex-col">
-                    <section className="bg-white shadow-default desktop:shadow-none mx-4 mb-6 rounded-[10px] px-6 py-8">
-                        <h1 className="text-blue800 desktop:text-[32px] desktop:mb-3 mb-2 text-[24px] font-bold leading-none">
+                <div className="flex flex-1 flex-col desktop:px-[100px] desktop:py-14 desktop:pb-8">
+                    <section className="mx-4 mb-6 rounded-[10px] bg-white px-6 py-8 shadow-default desktop:shadow-none">
+                        <h1 className="mb-2 text-[24px] font-bold leading-none text-blue800 desktop:mb-3 desktop:text-[32px]">
                             Personal info
                         </h1>
 
-                        <p className="text-body-l text-grey500 desktop:mb-9 mb-5">
+                        <p className="mb-5 text-body-l text-grey500 desktop:mb-9">
                             Please provide your name, email address, and phone number.
                         </p>
+
+                        <div>
+                            <Textfield label="name" placeholder="e.g. Stephen King" />
+                        </div>
                     </section>
 
-                    <footer className="bg-white shadow-default desktop:shadow-none mt-auto flex items-center justify-between p-4">
+                    <footer className="mt-auto flex items-center justify-between bg-white p-4 shadow-default desktop:shadow-none">
                         <Button variant="text">go back</Button>
                         <Button>next step</Button>
                     </footer>
---
+++ src/components/textfield.tsx
@@ -0,0 +1,26 @@
+import { twMerge } from 'tailwind-merge'
+
+type TextfieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
+    error?: string
+    label: string
+}
+
+export const Textfield = ({ className, error, label, ...others }: TextfieldProps) => {
+    return (
+        <label className="flex flex-col gap-1 desktop:gap-2">
+            <div className="flex items-center justify-between text-[14px] leading-none">
+                <span className="font-normal capitalize text-blue800">{label}</span>
+                {error != null && <span className="font-bold text-red500 ">{error}</span>}
+            </div>
+
+            <input
+                {...others}
+                className={twMerge(
+                    'rounded border-1 border-solid border-grey200 px-4 py-[10.5px] text-[15px] font-medium leading-none text-blue800 transition-colors placeholder:text-grey500 hover:border-purple500 focus:border-purple500 focus:outline-none desktop:rounded-lg desktop:py-[14px] desktop:text-[16px]',
+                    error != null && 'border-red500 hover:border-red500 focus:border-red500',
+                    className
+                )}
+            />
+        </label>
+    )
+}
